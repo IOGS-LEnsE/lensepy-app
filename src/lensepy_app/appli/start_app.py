4d862eacc3a9b9019a85f278c758f2f0ab4f8fb1
Julien Villemejane <julien.villemejane@institutoptique.fr>
2026-02-27 16:35:16 +0100

    Change file structure

--- src/lensepy_app/appli/start_app.py
+++ src/lensepy_app/appli/start_app.py
@@ -4,13 +4,13 @@ from pathlib import Path
 import lensepy
 from lensepy import translate, load_dictionary, dictionary
 
-from lensepy-app.appli._app.app_utils import XMLFileConfig, XMLFileModule
-from lensepy-app.appli._app.main_manager import MainManager
+from lensepy_app.modules.default.default_controller import DefaultController
+from lensepy_app.appli._app.app_utils import XMLFileConfig, XMLFileModule
+from lensepy_app.appli._app.main_manager import MainManager
 from PyQt6.QtWidgets import QApplication
 import importlib
 import importlib.util
 
-from lensepy-app.modules.default.default_controller import DefaultController
 
 
 class My_Application(QApplication):
