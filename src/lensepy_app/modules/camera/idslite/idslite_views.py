de43b856d71e6a60c2ea88cd843cf6049c22f5a0
Julien Villemejane <julien.villemejane@institutoptique.fr>
2026-09-18 18:47:57 +0200

    Update idslite_views.py

--- src/lensepy_app/modules/camera/idslite/idslite_views.py
+++ src/lensepy_app/modules/camera/idslite/idslite_views.py
@@ -53,7 +53,7 @@ class CameraParamsWidget(QWidget):
             layout.addWidget(make_hline())
             self.slider_black_level = SliderBloc(translate('ids_params_slider_black'), unit='ADU',
                                           min_value=0, max_value=255, integer=True)
-            #self.slider_black_level.slider.setEnabled(False)
+            self.slider_black_level.slider.setEnabled(False)
             layout.addWidget(self.slider_black_level)
             layout.addWidget(make_hline())
 
